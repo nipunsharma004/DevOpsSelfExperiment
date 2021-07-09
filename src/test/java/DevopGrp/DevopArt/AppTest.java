4015d4e9687aaef3bf6b4a192d0ccb9fd4593078
nipunsharma004 <84594079+nipunsharma004@users.noreply.github.com>
2021-07-09 05:39:23 -0700

    modified code checking auto build with multiple commits or push

--- src/test/java/DevopGrp/DevopArt/AppTest.java
+++ src/test/java/DevopGrp/DevopArt/AppTest.java
@@ -16,8 +16,8 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-        System.out.println("I am trying to trigger build in jenkins on each push via git to github, hoping to be successful");
-        System.out.println("no it didn't work as per push so i scheduled it based on a time in build trigger jenkins");
+        //System.out.println("I am trying to trigger build in jenkins on each push via git to github, hoping to be successful");
+        System.out.println("no above didn't work as per push so i scheduled it based on a time in build trigger jenkins");
         System.out.println("above worked hurray!");
         System.out.println("DevOps Testing Successful");
     }
